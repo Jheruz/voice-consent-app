861c087ff98725ccab6f2285f76b060ca9dcd624
Jheruz <jbdelacruz0702@gmail.com>
2022-09-12 07:23:52 -0400

    created TextInput component | update theme color and styles

--- App/theme/colors.js
+++ App/theme/colors.js
@@ -1,3 +1,4 @@
 export default {
   black: '#000',
+  gray: '#A9A9A9',
 }
--- App/theme/styles.js
+++ App/theme/styles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native'
 
 import themeColor from './colors'
 
-const globalSpacing = 20
+export const globalSpacing = 20
 
 export default StyleSheet.create({
   // fonts
@@ -11,8 +11,20 @@ export default StyleSheet.create({
     fontSize: 30,
     fontWeight: 'bold',
   },
+  label: {
+    color: themeColor.black,
+    fontSize: 20,
+  },
 
   // alignments
+  alignItemsCenter: {
+    alignItems: 'center',
+  },
+  alignItemsEnd: {
+    alignItems: 'flex-end',
+  },
+
+  // spacing
   contentTopSpacing: {
     paddingTop: globalSpacing * 3,
     paddingBottom: globalSpacing,
@@ -20,7 +32,16 @@ export default StyleSheet.create({
   pageHorizontalSpacing: {
     paddingHorizontal: globalSpacing,
   },
-  alignItemsCenter: {
-    alignItems: 'center',
+  spacingLeft: {
+    marginLeft: globalSpacing,
+  },
+  spacingLeftMedium: {
+    marginLeft: globalSpacing / 2,
+  },
+  spacingBottom: {
+    marginBottom: globalSpacing,
+  },
+  spacingBottomMedium: {
+    marginBottom: globalSpacing / 2,
   },
 })
